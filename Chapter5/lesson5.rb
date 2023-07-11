25255e9c3abe7c67a92866462a734e626c3be190
EC2 Default User <ec2-user@ip-172-31-11-114.ap-northeast-1.compute.internal>
2023-07-11 00:16:04 +0000

    [Start] 2syou

---
+++ Chapter5/lesson5.rb
@@ -0,0 +1,13 @@
+total_price = 38000
+
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり。"
+end
+
+if total_price == 100
+  puts "みかんを購入。所持金は０円。"
+end
+
+if total_price < 100
+  puts "みかんを購入する事ができません。"
+end
